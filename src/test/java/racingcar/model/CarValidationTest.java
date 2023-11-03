c7eb0ab55068617c014ba8f3073973448b2fe081
JaeMin-1 <39516729+JaeMin-1@users.noreply.github.com>
2023-11-03 22:14:23 +0900

    test: methodSource 적용

--- src/test/java/racingcar/model/CarValidationTest.java
+++ src/test/java/racingcar/model/CarValidationTest.java
@@ -6,54 +6,42 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CarValidationTest {
-    private CarValidation carValidation = new CarValidation();
-
     @Test
     void validateAll_correct() {
         // given
         List<String> validCarNames = Arrays.asList("Car1", "Car2", "Car3");
 
         // when
-        ArrayList<String> carNames = carValidation.carValidateAll(validCarNames);
+        ArrayList<String> carNames = CarValidation.carValidateAll(validCarNames);
 
         // then
         assertThat(carNames).containsExactly("Car1", "Car2", "Car3");
     }
 
-    @Test
-    void validateAll_too_longCarNames() {
-        // given
-        List<String> validCarNames = Arrays.asList("Car123454", "Car5", "Car1");
-
-        // when & then
-        assertThatThrownBy(() -> carValidation.carValidateAll(validCarNames))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차 이름이 5자 이하가 아닙니다: " + validCarNames.get(0));
+    static Stream<Arguments> invalidParameters() {
+        return Stream.of(
+                Arguments.of("Car1", "Car123454", "Car3", "자동차 이름이 5자 이하가 아닙니다: Car123454"),
+                Arguments.of("Car1", "Car@4", "Car3", "특수문자가 포함된 문자열이 입력되었습니다: Car@4"),
+                Arguments.of("Car1", "Car1", "Car2", "중복된 이름이 입력되었습니다: Car1")
+        );
     }
 
-    @Test
-    void validateAll_invalidCarNames() {
+    @ParameterizedTest
+    @MethodSource("invalidParameters")
+    void validateAll(String car1, String car2, String car3, String errorMessage) {
         // given
-        List<String> validCarNames = Arrays.asList("Car@4", "Car5$", "Car1");
+        List<String> validCarNames = Arrays.asList(car1, car2, car3);
 
         // when & then
-        assertThatThrownBy(() -> carValidation.carValidateAll(validCarNames))
+        assertThatThrownBy(() -> CarValidation.carValidateAll(validCarNames))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("특수문자가 포함된 문자열이 입력되었습니다: " + validCarNames.get(0));
+                .hasMessage(errorMessage);
     }
-
-    @Test
-    void testValidateDuplication() {
-        // given
-        List<String> validCarNames = Arrays.asList("Car2", "Car2", "Car1");
-
-        // when & then
-        assertThatThrownBy(() -> carValidation.carValidateAll(validCarNames))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("중복된 이름이 입력되었습니다: " + validCarNames.get(1));
-    }
-
 }

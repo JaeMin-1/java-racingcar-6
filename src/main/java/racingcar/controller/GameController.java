5faeb41f29d3dfdbfab46072d66f476cdd649eff
JaeMin-1 <39516729+JaeMin-1@users.noreply.github.com>
2023-11-03 21:14:52 +0900

    refacor: for문을 stream으로

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -9,41 +9,26 @@ import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class GameController {
-    public void runGame() {
-        List<String> carNames = getInputCarNames();
-        int tryNumber = getInputTryNumber();
+    public void runStart() {
+        List<String> inputArr = InputView.inputCarNames();
+        List<String> carNames = CarValidation.carValidateAll(inputArr);
+
+        String inputNumber = InputView.inputTryNumber();
+        int tryNumber = NumberValidation.numbervalidateAll(inputNumber);
+        System.out.println();
 
         Race race = new Race();
         race.initialize(carNames);
 
-        displayExecutionResultsHeader();
+        runRaceAndPrintResults(race, tryNumber);
+    }
+
+    private void runRaceAndPrintResults(Race race, int tryNumber) {
+        System.out.println(Constants.EXECUTION_RESULTS_HEADER.getValue());
         for (int i = 0; i < tryNumber; i++) {
             race.runRace();
-            displayExecutionResults(race.getCarNames(), race.getCarGoingCount());
+            System.out.println(OutputView.showExecutionResults(race.getCarNames(), race.getCarGoingCount()));
         }
-        displayWinner(race.winners());
-    }
-
-    private List<String> getInputCarNames() {
-        List<String> inputArr = InputView.inputCarNames();
-        return CarValidation.carValidateAll(inputArr);
-    }
-
-    private int getInputTryNumber() {
-        String inputNumber = InputView.inputTryNumber();
-        System.out.println();
-        return NumberValidation.numbervalidateAll(inputNumber);
-    }
-
-    private void displayExecutionResultsHeader() {
-        System.out.println(Constants.EXECUTION_RESULTS_HEADER);
-    }
-
-    private void displayExecutionResults(List<String> carNames, List<Integer> carGoingCount) {
-        System.out.println(OutputView.showExecutionResults(carNames, carGoingCount));
-    }
-
-    private void displayWinner(String winners) {
-        System.out.println(OutputView.showWinner(winners));
+        System.out.println(OutputView.showWinner(race.winners()));
     }
 }
--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringJoiner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Race {
     private List<String> carNames;
@@ -24,17 +26,14 @@ public class Race {
      * 무작위로 숫자를 자동차 대수만큼 추출해서 4 이상이면 한칸 전진
      */
     public void runRace() {
-        ArrayList<Integer> randomNumbersList = new ArrayList<>();
-        for (int j = 0; j < carNames.size(); j++) {
-            int randomNumber = Randoms.pickNumberInRange((int) Constants.MIN_RANDOM_NUMBER.getValue(),
-                    (int) Constants.MAX_RANDOM_NUMBER.getValue());
-            randomNumbersList.add(randomNumber);
-        }
-        for (int j = 0; j < randomNumbersList.size(); j++) {
-            if (randomNumbersList.get(j) >= (int) Constants.FORWARD_CONDITION.getValue()) {
-                carGoingCount.set(j, carGoingCount.get(j) + 1);
-            }
-        }
+        List<Integer> randomNumbersList = carNames.stream()
+                .map(name -> Randoms.pickNumberInRange((int) Constants.MIN_RANDOM_NUMBER.getValue(),
+                        (int) Constants.MAX_RANDOM_NUMBER.getValue()))
+                .collect(Collectors.toList());
+
+        IntStream.range(0, randomNumbersList.size())
+                .filter(j -> randomNumbersList.get(j) >= (int) Constants.FORWARD_CONDITION.getValue())
+                .forEach(j -> carGoingCount.set(j, carGoingCount.get(j) + 1));
     }
 
     public String winners() {
